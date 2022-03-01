92b9bc472678476e9751cf8c4f14ff370eaf50d5
Rahim Baharbin <r.baharbin@gmail.com>
2022-03-01 05:27:37 +0330

    Add project files.

---
+++ InternetCheck/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace InternetCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.topMostOnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.topMostOffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripSeparator1,
+            this.eXITToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(127, 54);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.topMostOnToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.topMostOffToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(126, 22);
+            this.toolStripMenuItem1.Text = "نوع نمایش";
+            // 
+            // topMostOnToolStripMenuItem
+            // 
+            this.topMostOnToolStripMenuItem.Name = "topMostOnToolStripMenuItem";
+            this.topMostOnToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.topMostOnToolStripMenuItem.Text = "Top Most On";
+            this.topMostOnToolStripMenuItem.Click += new System.EventHandler(this.topMostOnToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(140, 6);
+            // 
+            // topMostOffToolStripMenuItem
+            // 
+            this.topMostOffToolStripMenuItem.Name = "topMostOffToolStripMenuItem";
+            this.topMostOffToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.topMostOffToolStripMenuItem.Text = "Top Most Off";
+            this.topMostOffToolStripMenuItem.Click += new System.EventHandler(this.topMostOffToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(123, 6);
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.eXITToolStripMenuItem.Text = "EXIT";
+            this.eXITToolStripMenuItem.Click += new System.EventHandler(this.eXITToolStripMenuItem_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::InternetCheck.Properties.Resources.internet_nok;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(50, 50);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem topMostOnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topMostOffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ InternetCheck/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Diagnostics;
+
+namespace InternetCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Size = new Size(5,5);
+            Location = (Point)new Size(SystemInformation.WorkingArea.Width - Size.Width , SystemInformation.WorkingArea.Height - Size.Height);
+            Opacity = 0.0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+          //  timer1.Enabled = true;
+        }
+
+  
+
+        private void eXITToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+       
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int Flage = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                try
+                {
+                    if (new Ping().Send("8.8.8.8", 3000).Status == IPStatus.Success)
+                    {
+                        Flage = 0;
+                        BackgroundImage = Properties.Resources.internet_ok;
+                    }
+                    else
+                    {
+                        BackgroundImage = Properties.Resources.internet_nok;
+                        Flage++;
+                        if (Flage == 5)
+                        {
+                            Process p_cmd;
+                            if (new Ping().Send("192.168.1.1", 3000).Status == IPStatus.Success)
+                            {
+                                LaunchBrowser();
+                                p_cmd = new Process() { StartInfo = new ProcessStartInfo() { FileName = "cmd.exe", Arguments = "/c ping 8.8.8.8 -t" } };
+                                
+                            }
+                            else 
+                            {
+                                p_cmd = Process.Start(new ProcessStartInfo("NCPA.cpl") { UseShellExecute = true });
+                            }                            
+                            p_cmd.Start();
+                            bool _flage = true;
+                            while (_flage)
+                            {
+                                Process[] AllProcess = Process.GetProcesses(".");
+                                foreach (Process p_col in AllProcess)
+                                {
+                                    if (p_col.Id == p_cmd.Id)
+                                    {
+                                        _flage = true;
+                                        Thread.Sleep(5000);
+                                        break;
+                                    }
+                                    else
+                                        _flage = false;
+                                }
+                            }
+                            p_cmd = null;
+                        }
+
+                    }
+                }
+                catch
+                {
+                    Flage++;
+                    if (Flage == 2)
+                    {
+                        Thread.Sleep(5000);
+                        BackgroundImage = Properties.Resources.DataGridViweDefultImage_28;
+                        Process p = Process.Start(new ProcessStartInfo("NCPA.cpl") { UseShellExecute = true });
+                        bool flage = true;
+                        while (flage)
+                        {
+                            Process[] AllProcess = Process.GetProcesses(".");
+                            foreach (Process p_col in AllProcess)
+                            {
+                                if (p_col.Id == p.Id)
+                                {
+                                    flage = true;
+                                    Thread.Sleep(5000);
+                                    break;
+                                }
+                                else
+                                    flage = false;
+                            }
+                        }
+                        p = null;
+                    }
+                }
+            
+                // _ = Invoke((MethodInvoker)delegate ()
+                //{
+                //    if (Status)
+                //        pictureBox1.Image = InternetCheck.Properties.Resources.ok;
+                //    else
+                //        pictureBox1.Image = InternetCheck.Properties.Resources.not;
+
+                //});
+
+            }
+        }
+        private Process LaunchBrowser()
+        {
+            string browserName = "iexplore.exe";
+            using (Microsoft.Win32.RegistryKey userChoiceKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice"))
+            {
+                if (userChoiceKey != null)
+                {
+                    string[] name = userChoiceKey.GetValueNames();
+                    foreach (string item in name)
+                    {
+                        if (item.ToLower().Contains("progid"))
+                        {
+                            object progIdValue = userChoiceKey.GetValue(item);
+                            if (progIdValue != null)
+                            {
+                                if (progIdValue.ToString().ToLower().Contains("chrome"))
+                                    browserName = "chrome.exe";
+                                else if (progIdValue.ToString().ToLower().Contains("firefox"))
+                                    browserName = "firefox.exe";
+                                else if (progIdValue.ToString().ToLower().Contains("safari"))
+                                    browserName = "safari.exe";
+                                else if (progIdValue.ToString().ToLower().Contains("opera"))
+                                    browserName = "opera.exe";
+                                else if (progIdValue.ToString().ToLower().Contains("msedge"))
+                                    browserName = "msedge.exe";
+                                else if (progIdValue.ToString().ToLower().Contains("yandex"))
+                                    browserName = "browser.exe";
+                            }
+                        }
+                    }
+
+                }
+            }
+            try
+            {
+                return Process.Start(browserName, "192.168.1.1");
+            }
+            catch
+            {
+                return Process.Start("iexplore.exe", "192.168.1.1");
+            }
+
+        }
+        private void topMostOnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopMost = true;
+        }
+
+        private void topMostOffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopMost = false;
+        }
+    }
+}
+
